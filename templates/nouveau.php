46b07ec404c63d539285ac21e815ea47ca9dcdc2
Johanna Fricon <johanna.fricon@outlook.fr>
2020-03-09 18:51:31 +0300

    Ajout de enregistrement et nouveau dans src et template

---
+++ templates/nouveau.php
@@ -0,0 +1,103 @@
+
+
+<html>
+<head>
+<title>nouveau client</title>
+ <link rel="stylesheet" type="text/css" href="../styles/Normalize.css">
+    <link rel="stylesheet" type="text/css" href="../styles/Header.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" type=""></script>
+
+
+
+  </head>
+
+  <body>
+    
+    <header>
+
+          <nav class="menu">
+
+              <div class="inner">
+
+                <div class="titrebox">
+
+                  <h1 class="titre">Fitness News</h1>
+
+                </div>
+
+                <div class="navbox">
+
+                  <a href="../index.html"><i class="fa fa-home" aria-hidden="true"></i> Accueil</a> 
+                  <a href="Connexion.html"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a>
+                  <a href="Statistiques.html"><i class="fa fa-calculator" aria-hidden="true"></i> Statistiques</a>
+                  <a href="Contact.html"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</a>
+                  <a href="A propos.html"><i class="fa fa-question-circle" aria-hidden="true"></i> A propos</a>
+
+                </div>
+
+                <div class="toggle">
+                
+                  <span class="toggleicons"></span>
+
+                </div>
+
+              </div>
+
+
+          </nav>
+
+    </header>
+
+    <script src="scripts/app.js" type="text/javascript"></script>
+
+    <div class="container-pers">
+      <form action="../src/enregistrement.php" method="get" autocomplete="off">
+<p>
+Nom :
+<input type="text" name="n" value=""/>
+</p>
+<p>
+Prénom :
+<input type="text" name="p" value=""/>
+</p>
+
+<p>
+Adresse email :
+<input type="mail" name="maill" value=""/>
+</p>
+
+<p>
+numéro de telephone  :
+<input type="int" name="num" value=""/>
+</p>
+
+<p>
+Mot de passe :
+<input type="password" name="mdp1" value=""/>
+</p>
+
+<p>
+Confirmez le mot de passe :
+<input type="password" name="mdp2" value=""/>
+</p>
+<p>
+<input type="submit" value="Envoyer">
+</p>
+
+
+
+
+</form>
+    </div>
+
+    
+
+
+    <footer>
+      JE SUIS DU CONTENU ET JE SUIS DANS CHACUNE DES PAGES DONC MON CONTENU EST LE MEME DANS CHAQUE PAGE
+    </footer> 
+	
+
+	 </body>
+</html>
