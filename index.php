cc7ce1fc6c30bc2bb3e3573ae8ea8d346cbc520f
Yacine <Yacine.Akki@etu.univ-montp3.fr>
2020-03-18 14:56:34 +0100

    Le grand remplacement nouvelle barre de navigation

--- index.php
+++ index.php
@@ -1,114 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
-  <head>
-
+<head>
     <meta charset="utf-8">
-
-    <title>FitnessNews.com</title>
-
-    <link rel="stylesheet" type="text/css" href="styles/Footer.css">
-    <link rel="stylesheet" type="text/css" href="styles/Header.css">
+    <title>Body Check</title>
+    <link rel="stylesheet" href="style/header.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" type=""></script>
+</head>
 
-  </head>
-
-  <body>
-    
+<body>
     <header>
-
-          <nav class="menu">
-              
-            <div class="inner">
-
-                <div class="titrebox">
-
-                  <h1 class="titre">Fitness News</h1>
-
-                </div>
-
-                <div class="navbox">
-
-                  <a href="index.html"><i class="fa fa-home" aria-hidden="true"></i> Accueil</a>
-                  <a href="templates/Statistiques.php"><i class="fa fa-calculator" aria-hidden="true"></i> Statistiques</a>
-                  <a href="templates/A propos.php"><i class="fa fa-question-circle" aria-hidden="true"></i> A propos</a>
-                  <a href="templates/Contact.php"><i class="fa fa-envelope" aria-hidden="true"></i> Support</a>
-                  <a href="templates/nouveau.php"><i class="fa fa-clipboard" aria-hidden="true"></i> S'inscrire</a> 
-                  <a href="templates/Connexion.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a>
-
-                </div>
-
-                <div class="toggle">
-                
-                  <span class="toggleicons"></span>
-
-                </div>
-
-              </div>
-
-
-          </nav>
-
-    </header>
-
-    <script src="scripts/app.js" type="text/javascript"></script>
-
-    
-
-    <section class="container-pers">
-      JE SUIS LE CONTENU PROPRE DE CHACUNE DES PAGES JE NE SUIS PAS LA MEME CHOSE
-      QUE LES AUTRES
-    </section>
-
-    
-
-
-    <footer class="bas-page">
-      
-        <div class="section-foot">
-          du text ici
+        <div class="logo-container">
+            <a href="index.php"><img src="ressources/logo.png" alt="description" id="logo"></a>
         </div>
-
-        <div class="section-foot">
-          fghfjj
+        <div class="container-menu-nav">
+            <nav class="top-menu-nav">
+                <ul class="top-menu">
+                    <li><a href="index.php" class="link">Accueil <i class="fa fa-home" aria-hidden="true"></i></a></li>
+                    <li><a href="templates/test.php" class="link">Test <i class="fa fa-list" aria-hidden="true"></i></a></li>
+                    <li><a href="templates/apropos.php" class="link">A propos <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></li>
+                    <li><a href="templates/contact.php" class="link">Contact <i class="fa fa-address-book-o" aria-hidden="true"></i></a></li>
+                    <li id="nav-moncompte"><a href="templates/moncompte.php" class="link">Mon Compte <i class="fa fa-user-circle-o" aria-hidden="true"></i></a></li>
+                    <li id="lang"><a><i class="fa fa-globe" aria-hidden="true"></i> FR <span id="eur">(EUR)</span></a></li>
+                </ul>
+            </nav>
+            <div class="toggle"><span></span></div>
         </div>
+      </header>
 
-        <div class="section-foot">
-          jyrjyyj
-        </div>
-
-
-      
-    </footer> 
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  </body>
+</body>
 </html>
