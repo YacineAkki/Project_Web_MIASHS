5239abefe5331fc0bf3a028aebe821379cfa86ca
Yacine <Yacine.Akki@etu.univ-montp3.fr>
2020-03-20 21:03:22 +0100

    modif mineurs

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Body Check</title>
     <link rel="stylesheet" href="style/header.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" type=""></script>
 </head>
 
 <body>
