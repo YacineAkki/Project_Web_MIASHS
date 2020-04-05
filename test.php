c79b5abb7cad7d99cfa61eb4e5934758e66f4dc4
Moulikatou <moulikahip@gmail.com>
2020-04-05 22:15:23 +0200

    another bach of modifications

--- test.php
+++ test.php
@@ -6,18 +6,20 @@
 
 	<head>
 		<title>Test</title>
-		<meta name="generator" content="BBEdit 13.0" />
+		
 		<link rel="stylesheet" href="styles/test.css" type="text/css" media="screen"/>
 	</head>
 	<body>
 
 		<form action="enregistrementTest.php" method="get" autocomplete="off">
-			<h1>Commencer le test</h1>
-				<p> Salut! Je suis le professeur Test. C'est moi qui vais m'occuper de votre Body Check... euhh 
-					c'est plus un Nutri Check, si vous voyer ce que je veux dire.</p>
-				<p>Pour pouvoir effectuer mes calculs j'ai besoins de vous connaitre vous et vos habitudes alimentaires.</p>
-				<br/> 
-			<h2> Commençons par vous : </h2>
+			<div>
+				<h1>Commencer le test</h1>
+					<p> Salut! Je suis le professeur Test. C'est moi qui vais m'occuper de votre Body Check... euhh 
+						c'est plus un Nutri Check, si vous voyer ce que je veux dire.</p>
+					<p>Pour pouvoir effectuer mes calculs j'ai besoins de vous connaitre vous et vos habitudes alimentaires.</p>
+					<br/> 
+				<h2> Commençons par vous : </h2>
+			</div>
 
 			<div>
 				Quel est votre date de naissance ? :
@@ -42,7 +44,100 @@
             	  <label>Votre poids :</label>
                  <input type="number" name="poids" value= "">
 
-             </div>
+            </div>
+
+			 <!-- plat type journalier -->
+			<div>
+			
+					<p>Maintenant que les présentations sont faites, c'est le moment de voir plus en détails ce que vous consommer par jour.</p>
+					<br/> 
+					<p>Donnez-moi, un exemple de repas (plats, boissons, snacks etc ...) que vous manger aux moments de la journée ci-dessous :</p>
+					<br/> 
+					
+				<h2> Le matin : </h2>
+		
+			</div>
+
+			<div class="input">
+				Que prenez- vous les matins?
+            	  <label> repas :</label>
+                 <input type="text" name="matin" value= "">
+
+            </div>
+
+			<div class="input">
+				Que prenez- vous à midi?
+            	  <label> repas :</label>
+                 <input type="text" name="matin" value= "">
+
+            </div>
+
+			<div class="input">
+				Snack time
+            	  <label> repas :</label>
+                 <input type="text" name="matin" value= "">
+				
+
+            </div>
+
+			<div class="input">
+				Que prenez- vous à le soir?
+            	  <label> repas :</label>
+                 <input type="text" name="matin" value= "">
+
+            </div>
+
+			<!-- Habitudes de vie -->
+			
+			<div>
+			
+					<p>C'est parfait! Interessons-nous un peu à vos habitudes de vie</p>
+					<br/> 
+					
+					
+				<h2> Habitudes de vie</h2>
+		
+			</div>
+
+			<div class="input">
+				Êtes-vous un fumeur?  
+            	  <label>fumeur:</label>
+                 <input type="boolean" name="fumer" value= "">
+
+				<?php 
+
+					$fumer = $_POST['fumer'];
+						if( $fumer == True){
+							echo'à quel fréquence ?';
+							echo'<label>fréquence:</label>
+							<input type="number" name="frequence" value= "">';
+						}
+					
+				?>
+
+            </div>
+
+			<div class="input">
+				Pratiquez vous un sport?  
+            	  <label>Sport:</label>
+                 <input type="boolean" name="sport" value= "">
+
+				<?php 
+					$sport = $_POST['sport'];
+						if( $sport == True){
+
+							echo'Lequel ?';
+							echo'<label>Nom du Sport:</label>
+							<input type="text" name="nomSport" value= "">';
+
+							echo'combien de fois par semaine ?';
+							echo'<label>fréquence Sport:</label>
+							<input type="number" name="frequenceSport" value= "">';
+						}
+					
+				?>
+
+            </div>
 		</form>
 
 
