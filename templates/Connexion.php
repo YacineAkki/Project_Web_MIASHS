3e019133e3d2f33cfc05c0400ea8474dc0cb8a8d
Justin <jchikhaoui@live.fr>
2020-04-06 11:09:57 +0200

    Lien arrangé;ajout texte index+apropos

--- templates/Connexion.php
+++ templates/Connexion.php
@@ -1,52 +1,50 @@
 <!DOCTYPE html>
 
 
-        <html>
-           
+<html>
 
-            <head>
-                <meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
-	            <title>Connexion</title>
-	            <link rel="stylesheet" href="../style/connexionRelated.css" type="text/css" media="screen"/>
-                <link rel="stylesheet" href="../style/model.css">
-                <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-                <script src="../scripts/jquery-3.4.1.min.js"></script>
-                <script src="../scripts/app.js" type="text/javascript"></script>
-            </head>
-            <body>
-                <header>
-                    <div class="logo-container">
-                        <a href="index.php"><img src="ressources/logo.png" alt="description" id="logo"></a>
-                    </div>
-                    <div class="container-menu-nav">
-                        <nav class="top-menu-nav">
-                            <ul class="top-menu">
-                                <li><a href="index.php" class="link">Accueil <i class="fa fa-home" aria-hidden="true"></i></a></li>
-                                <li><a href="templates/test.php" class="link">Test <i class="fa fa-list" aria-hidden="true"></i></a></li>
-                                <li><a href="templates/apropos.php" class="link">A propos <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></li>
-                                <li><a href="templates/contact.php" class="link">Contact <i class="fa fa-address-book-o" aria-hidden="true"></i></a></li>
-                                <li id="nav-moncompte"><a href="templates/moncompte.php" class="link">Mon Compte <i class="fa fa-user-circle-o" aria-hidden="true"></i></a></li>
-                                <li id="lang"><a><i class="fa fa-globe" aria-hidden="true"></i> FR <span id="eur">(EUR)</span></a></li>
-                            </ul>
-                        </nav>
-                        <div class="toggle"><span></span></div>
-                    </div>
-                </header>
+    <head>
+        <meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../style/connexionRelated.css" type="text/css" media="screen"/>
+        <link rel="stylesheet" href="../style/model.css">
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="../scripts/jquery-3.4.1.min.js"></script>
+        <script src="../scripts/app.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+
+        <header>
+            <div class="logo-container">
+                <a href="index.php"><img src="ressources/logo.png" alt="description" id="logo"></a>
+            </div>
+                <div class="container-menu-nav">
+                    <nav class="top-menu-nav">
+                        <ul class="top-menu">
+                            <li><a href="../index.php" class="link">Accueil <i class="fa fa-home" aria-hidden="true"></i></a></li>
+                            <li><a href="test.php" class="link">Test <i class="fa fa-list" aria-hidden="true"></i></a></li>
+                            <li><a href="apropos.php" class="link">A propos <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></li>
+                            <li><a href="contact.php" class="link">Contact <i class="fa fa-address-book-o" aria-hidden="true"></i></a></li>
+                            <li id="nav-moncompte"><a href="Connexion.php" class="link">Mon Compte <i class="fa fa-user-circle-o" aria-hidden="true"></i></a></li>
+                            <li id="lang"><a><i class="fa fa-globe" aria-hidden="true"></i> FR <span id="eur">(EUR)</span></a></li>
+                        </ul>
+                    </nav>
+                <div class="toggle"><span></span></div>
+            </div>
+        </header>
                 
-                <div id="container">
-                    
-
-                    <!-- saisi des inputs --> 
-                    <form action="se_connecter.php" method="post" autocomplete="off">
-                        <h1> Connexion </h1>
-                        <div class="input">
-                            <label>Adresse e-mail :</label>
-                            <?php
-
-                                echo '<input type="text" name="mail" value ='. $_POST['mail']. '/>';
-
-                            ?>
-                        </div>
+        <div id="container">
+            
+            <!-- saisi des inputs --> 
+            <form action="se_connecter.php" method="post" autocomplete="off">
+                <h1> Connexion </h1>
+                <div class="input">
+                    <label>Adresse e-mail :</label>
+                    <?php
+                       echo '<input type="text" name="mail" value ='. $_POST['mail']. '/>';
+                    ?>
+                </div>
 
 
                         <div class="input">
@@ -69,17 +67,11 @@
 
                     
                 </div>
-                <footer>
-                    <ul><h3>Université Paul Valéry</h3>
-                        
-                    </ul>
-                    <ul><h3>Qui sommes nous ?</h3>
-                        
-                    </ul>
-                    <ul><h3>A Propos</h3>
-                        
-                    </ul>
-                </footer>  
+    <footer>
+        <ul> <h3>Université Paul Valéry</h3> </ul>
+        <ul> <h3>Qui sommes nous ?</h3> </ul>
+        <ul> <h3>A Propos</h3> </ul>
+    </footer>
 
             </body>
         </html>
